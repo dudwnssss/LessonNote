ccec03048412ca67d38caaf6acf88201f139dd82
leolim <dudwnssss@naver.com>
2023-10-01 14:50:23 +0900

    init: project init

---
+++ LessonNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LessonNote
+//
+//  Created by 임영준 on 2023/10/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
